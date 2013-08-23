9ede8d4c44e727e8a121754dd31c7ad82890f6f9
Kevin Tindall <kevinkjt2000@gmail.com>
2013-08-22 19:32:52 -0500

    Silly changes like sending the player flying in the air when the block is placed.

--- common/kgbostian/mods/solarpowermod/blocks/SolarGenerator.java
+++ common/kgbostian/mods/solarpowermod/blocks/SolarGenerator.java
@@ -7,7 +7,10 @@ import kgbostian.mods.solarpowermod.SolarPowerModBlocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class SolarGenerator extends Block
@@ -15,13 +18,30 @@ public class SolarGenerator extends Block
    public SolarGenerator(int par1)
    {
       super(par1, Material.iron);
+      init();
+   }
+   
+   private void init()
+   {
+      this.setCreativeTab(CreativeTabs.tabBlock);
+      this.setHardness(-1.0f);
+      this.setResistance(5.0f);
+      this.setUnlocalizedName("solargenerator");
+      this.setStepSound(soundGrassFootstep);
    }
    
    @Override
-   public void onBlockClicked(World world, int x, int y, int z,
-         EntityPlayer player)
+   public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player)
    {
-      player.addChatMessage("You clicked a solar generator!");
+      world.getWorldLogAgent().logInfo(player.getEntityName() + " clicked a solar engine!");
+   }
+   
+   @Override
+   public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase elb, ItemStack itemStack)
+   {
+      EntityPlayer player = world.getPlayerEntityByName(elb.getEntityName());
+      world.getWorldLogAgent().logInfo(player.getEntityName() + " went flying!");
+      player.addVelocity(0, 4, 0);
    }
    
    @Override
