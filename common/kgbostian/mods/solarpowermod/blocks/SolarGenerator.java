0bffc5cd00265b12d88d116d4902e3215cb3a5bd
Kevin Tindall <kevinkjt2000@gmail.com>
2013-08-30 04:46:25 -0500

    Began work on integrating with BuildCraft power

--- common/kgbostian/mods/solarpowermod/blocks/SolarGenerator.java
+++ common/kgbostian/mods/solarpowermod/blocks/SolarGenerator.java
@@ -2,6 +2,11 @@ package kgbostian.mods.solarpowermod.blocks;
 
 import java.util.Random;
 
+import buildcraft.api.power.IPowerEmitter;
+import buildcraft.api.power.IPowerReceptor;
+import buildcraft.api.power.PowerHandler;
+import buildcraft.api.power.PowerHandler.PowerReceiver;
+
 import kgbostian.mods.solarpowermod.SolarPowerMod;
 import kgbostian.mods.solarpowermod.SolarPowerModBlocks;
 
@@ -12,9 +17,13 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 
-public class SolarGenerator extends Block
+public class SolarGenerator extends Block implements IPowerEmitter, IPowerReceptor
 {
+   private PowerHandler powerHandler;
+   public float energy;
+   
    public SolarGenerator(int par1)
    {
       super(par1, Material.iron);
@@ -25,9 +34,10 @@ public class SolarGenerator extends Block
    {
       this.setCreativeTab(CreativeTabs.tabBlock);
       this.setHardness(-1.0f);
-      this.setResistance(5.0f);
-      this.setUnlocalizedName("solargenerator");
+      this.setResistance(10.0f);
+      this.setUnlocalizedName("solarGenerator");
       this.setStepSound(soundGrassFootstep);
+      this.func_111022_d("solar_engine");
    }
    
    @Override
@@ -48,5 +58,43 @@ public class SolarGenerator extends Block
    public int idDropped(int par1, Random par2Random, int par3)
    {
       return SolarPowerModBlocks.solargenerator.blockID;
+   }
+
+   @Override
+   public boolean canEmitPowerFrom(ForgeDirection side)
+   {
+      return true;
+   }
+
+   @Override
+   public PowerReceiver getPowerReceiver(ForgeDirection side)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void doWork(PowerHandler workProvider)
+   {
+      addEnergy(powerHandler.useEnergy(1, maxEnergyReceived(), true) * 0.95F);
+   }
+
+   private void addEnergy(float f)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   private float maxEnergyReceived()
+   {
+      
+      return 0;
+   }
+
+   @Override
+   public World getWorld()
+   {
+      // TODO Auto-generated method stub
+      return null;
    }
 }
